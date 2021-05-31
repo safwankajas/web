91efdb0665b5cf95de432bee09cbc86f0e2294b0
safwankajas <84768183+safwankajas@users.noreply.github.com>
2021-05-31 16:04:59 +0530

    about complected

--- script.js
+++ script.js
@@ -2,8 +2,11 @@ $(document).ready(function(){
     $(window).scroll(function(){
         if(this.scrollY > 20) {
             $('.navabar').addClass("sticky");
+            $('.home').addClass("sticky");
         }else{
             $('.navabar').removeClass("sticky");
+            $('.home').removeClass("sticky");
+
         }
     });
 
